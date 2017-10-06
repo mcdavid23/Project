1087067dbd08b28dad364460a8ff7cd3dc14ff99
TonyHanzhiSU <su.342@osu.edu>
2017-10-06 17:17:59 -0400

    Add the log message to show the lifecycle method get triggered

--- app/src/main/java/com/gif/gif/MainActivity.java
+++ app/src/main/java/com/gif/gif/MainActivity.java
@@ -2,6 +2,7 @@ package com.gif.gif;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v("gif", "onCreate triggered");
     }
+
+    protected void onStart() {
+        super.onStart();
+        Log.v("gif", "onStart triggered");
+    }
+
+    protected void onResume(){
+        super.onResume();
+        Log.v("gif", "onResume triggered");
+    }
+
+    protected void onPause(){
+        super.onPause();
+        Log.v("gif", "onPause triggered");
+    }
+
+    protected void onStop(){
+        super.onStop();
+        Log.v("gif", "onStop triggered");
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.v("gif", "onDestory triggered");
+    }
+
+
 }
