e03140c2146267806c01f42c9dfa691d3afa1b18
Eid Al-rabadi <eidal-rabadi@Eids-MacBook-Pro.local>
2017-11-05 16:12:01 -0500

    added select multiple images

--- app/src/main/java/com/gif/gif/MainActivity.java
+++ app/src/main/java/com/gif/gif/MainActivity.java
@@ -1,103 +1,81 @@
 package com.gif.gif;
-import android.app.Activity;
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
-import android.net.Uri;
-import android.view.View;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
-ArrayList<Uri> imageList = new ArrayList<Uri>();
-
-    private ShakeSensor mShakeSensor;
-    private SensorManager mSensorManager;
-    private Sensor mAccelerometer;
-
+    private LinearLayout lnrImages;
+    private Button btnAddPhots;
+    private Button btnSaveImages;
+    private ArrayList<String> imagesPathList;
+    private Bitmap yourbitmap;
+    private Bitmap resized;
+    private final int PICK_IMAGE_MULTIPLE =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // ShakeSensor initialization
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mShakeSensor = new ShakeSensor(new ShakeSensor.OnShakeListener() {
-            @Override
-            public void onShake() {
-                // SHARE
-                finish();
-            }
-        });
-
-        Log.v("gif", "onCreate triggered");
+        lnrImages = (LinearLayout)findViewById(R.id.lnrImages);
+        btnAddPhots = (Button)findViewById(R.id.btnAddPhots);
+        btnSaveImages = (Button)findViewById(R.id.btnSaveImages);
+        btnAddPhots.setOnClickListener(this);
+        btnSaveImages.setOnClickListener(this);
     }
-
-
-    public void openGallery(View v){
-        if (v.getId() == R.id.GALLERY){
-            Intent intent = new Intent();
-            intent.setType("image/*");
-            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-            intent.setAction(Intent.ACTION_GET_CONTENT);
-            startActivityForResult(Intent.createChooser(intent,"Select Picture"), 1);
-
-    }
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        if (resultCode == RESULT_OK) {
-            Uri selectedMediaUri = data.getData();
-            if (selectedMediaUri.toString().contains("image")) {
-                //handle image
-            } else if (selectedMediaUri.toString().contains("video")) {
-                //handle video
-            }
-
-            imageList.add(selectedMediaUri);
-            Log.v("Array list Size:", Integer.toString(imageList.size()));
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnAddPhots:
+                Intent intent = new Intent(MainActivity.this,CustomPhotoGalleryActivity.class);
+                startActivityForResult(intent,PICK_IMAGE_MULTIPLE);
+                break;
+            case R.id.btnSaveImages:
+                if(imagesPathList !=null){
+                    if(imagesPathList.size()>1) {
+                        Toast.makeText(MainActivity.this, imagesPathList.size() + " no of images are selected", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(MainActivity.this, imagesPathList.size() + " no of image are selected", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(MainActivity.this," no images are selected", Toast.LENGTH_SHORT).show();
+                }
+                break;
         }
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if(requestCode == PICK_IMAGE_MULTIPLE){
+                imagesPathList = new ArrayList<String>();
+                String[] imagesPath = data.getStringExtra("data").split("\\|");
+                try{
+                    lnrImages.removeAllViews();
+                }catch (Throwable e){
+                    e.printStackTrace();
+                }
+                for (int i=0;i<imagesPath.length;i++){
+                    imagesPathList.add(imagesPath[i]);
+                    yourbitmap = BitmapFactory.decodeFile(imagesPath[i]);
+                    ImageView imageView = new ImageView(this);
+                    imageView.setImageBitmap(yourbitmap);
+                    imageView.setAdjustViewBounds(true);
+                    lnrImages.addView(imageView);
+                }
+            }
+        }
 
-    protected void onStart() {
-        super.onStart();
-        Log.v("gif", "onStart triggered");
     }
-
-    protected void onResume(){
-        super.onResume();
-        mSensorManager.registerListener(mShakeSensor, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
-
-        Log.v("gif", "onResume triggered");
-    }
-
-    protected void onPause(){
-        super.onPause();
-        mSensorManager.unregisterListener(mShakeSensor);
-        Log.v("gif", "onPause triggered");
-    }
-
-    protected void onStop(){
-        super.onStop();
-        Log.v("gif", "onStop triggered");
-    }
-
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.v("gif", "onDestory triggered");
-    }
-
 
 }
