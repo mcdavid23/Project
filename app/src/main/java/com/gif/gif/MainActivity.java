a9f09d4bd46c697219af74ccdc5182d4c8c506b3
Eid Al-rabadi <eidal-rabadi@Eids-MacBook-Pro.local>
2017-11-04 20:13:39 -0400

    Open gallery

--- app/src/main/java/com/gif/gif/MainActivity.java
+++ app/src/main/java/com/gif/gif/MainActivity.java
@@ -1,12 +1,20 @@
 package com.gif.gif;
+import android.net.Uri;
 import android.view.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.content.Intent;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+ArrayList<Uri> imageList = new ArrayList<Uri>();
+
 
 
 
@@ -15,29 +23,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Log.v("gif", "onCreate triggered");
     }
 
 
-    public void onButtonClick(View v)
-    {
-        if(v.getId() == R.id.CREATE)
-        {
-            Intent i = new Intent(MainActivity.this,create.class);
-            startActivity(i);
-
-
-        }
-
+    public void openGallery(View v){
+        if (v.getId() == R.id.GALLERY){
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent,"Select Picture"), 1);
 
     }
+    }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
+        if (resultCode == RESULT_OK) {
+            Uri selectedMediaUri = data.getData();
+            if (selectedMediaUri.toString().contains("image")) {
+                //handle image
+            } else if (selectedMediaUri.toString().contains("video")) {
+                //handle video
+            }
 
-
-
-
-
+            imageList.add(selectedMediaUri);
+            Log.v("Array list Size:", Integer.toString(imageList.size()));
+        }
+    }
 
     protected void onStart() {
         super.onStart();
